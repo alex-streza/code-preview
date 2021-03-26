09ba07d75ca53cfa42685f202d8c81d27ddf365e
alex-streza <alex.streza@okapistudio.com>
2021-03-26 16:39:45 +0200

    add logs for webpack loaded files

--- src/utils/sourceLoader.js
+++ src/utils/sourceLoader.js
@@ -5,7 +5,9 @@ module.exports = function sourceLoader(source) {
   const { roots = [], compiled } = opts;
   const path = this.resourcePath;
   for (const root of roots) {
+    console.log(`root, path`, root, path);
     if (!root || (path.includes(root) && !path.includes("node_modules"))) {
+      console.log(`loaded file`, root, path);
       cache.register(
         path.substr(path.indexOf(root) + root.length),
         source,
