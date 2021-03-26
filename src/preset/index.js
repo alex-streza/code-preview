e4631b6fb39da6b482919d8dc2358ebc2e369415
alex-streza <alex.streza@okapistudio.com>
2021-03-26 13:28:25 +0200

    update package.json

--- src/preset/index.js
+++ src/preset/index.js
@@ -1,7 +1,32 @@
-export function config(entry = []) {
-  return [...entry, require.resolve("./preview")]
+const path = require("path");
+const CodePlugin = require("./utils/CodePlugin");
+
+module.exports = {
+  config: function (entry = []) {
+    return [...entry, require.resolve("./preview")]
+  },
+  managerEntries: function (entry = []) {
+    return [...entry, require.resolve("./manager")]
+  },
+  webpackFinal: async function (config, { configType }) {
+    config.module.rules.push({
+      test: /\.jsx?$|\.tsx?$|\.ts$|\.css$/,
+      use: [
+        {
+          loader: path.resolve(__dirname, "utils/sourceLoader.js"),
+          options: { roots: [path.resolve(__dirname, "../components/")] },
+        },
+      ],
+    });
+    config.plugins.push(new CodePlugin());
+    // prevent filename mangling (which b0rks source file switching)
+    config.mode = "development";
+    // prevent minification
+    config.optimization.minimizer = [];
+    // hide warnings
+    config.stats = { warnings: false };
+    // Return the altered config
+    return config;
+  }
 }
 
-export function managerEntries(entry = []) {
-  return [...entry, require.resolve("./manager")]
-}
